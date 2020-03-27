f983d753e3aab95ac82e6bb4b27a8bedc45f77a3
Billy Argenta <gustavo-argenta@hotmail.com>
2020-03-27 11:26:15 -0300

    Alteracoes no backend

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -34,7 +34,7 @@ export default function Detail() {
   }
   function sendWhatsapp() {
     Linking.openURL(
-      `whatsapp://send?phone=${incident.whatsapp}&text=${message}`
+      `whatsapp://send?phone=55${incident.whatsapp}&text=${message}`
     );
   }
   return (
